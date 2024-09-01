fe7527b50defb287d75bd7aa6ae06932fd6b02ba
Mriganka Shekhar Barman <mriganka.sb2018@gmail.com>
2024-09-02 01:06:15 +0530

    added html, js and css

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// You can add interactivity to your portfolio using JavaScript here
+
+// Example: Smooth scroll for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
